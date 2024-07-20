a4e2cc39fb4cd36fcc473c0be44bfbd6d868e97a
Emilee Hatch <hatch.emilee.cm@gmail.com>
2024-07-20 13:31:31 -0600

    Created components, and installed bootstrap.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+
+
+// Components
+import { HeaderComponent } from './core/layout/header/header.component';
+import { NavComponent } from './core/layout/nav/nav.component';
+import { MainComponent } from './core/layout/main/main.component';
+import { FooterComponent } from './core/layout/footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    MainComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
